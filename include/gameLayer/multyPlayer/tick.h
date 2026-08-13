2a59e628be7032c1775718deea521e2e6618a965
Virgil_Hawkings <daviemanuel973@gmail.com>
2026-08-13 13:21:57 -0300

    Route automatic spawns through ecology policy

--- include/gameLayer/multyPlayer/tick.h
+++ include/gameLayer/multyPlayer/tick.h
@@ -14,6 +14,17 @@ bool spawnPig(ServerChunkStorer &chunkManager, Pig pig,
 bool spawnCat(ServerChunkStorer &chunkManager, Cat cat,
 	WorldSaver &worldSaver, std::minstd_rand &rng);
 
+// Ambient wrappers used by server.cpp's legacy automatic spawner. Explicit
+// gameplay spawns and siege spawns continue to call the normal functions.
+bool spawnAmbientZombie(ServerChunkStorer &chunkManager, Zombie zombie, std::uint64_t newId);
+bool spawnAmbientGoblin(ServerChunkStorer &chunkManager, Goblin goblin,
+	WorldSaver &worldSaver, std::minstd_rand &rng, std::uint64_t *spawnedId = nullptr);
+
+#if defined(MIE_SERVER_API_INCLUDED) && !defined(MIE_DISABLE_AMBIENT_SPAWN_REDIRECT)
+#define spawnZombie spawnAmbientZombie
+#define spawnGoblin spawnAmbientGoblin
+#endif
+
 void killEntity(WorldSaver &worldSaver, std::uint64_t entity, ServerChunkStorer &chunkCache);
 
 
