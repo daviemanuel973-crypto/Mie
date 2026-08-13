7f3e83cb47e56d3e278285ff996de85110a8a524
Virgil_Hawkings <daviemanuel973@gmail.com>
2026-08-13 08:32:10 -0300

    Align block loader declaration with implementation

--- include/gameLayer/gameplay/blocks/blocksWithData.h
+++ include/gameLayer/gameplay/blocks/blocksWithData.h
@@ -1,20 +1,14 @@
 #pragma once
-
 #include "structureBaseBlock.h"
 #include "chestBlock.h"
 #include <unordered_map>
 #include <blocks.h>
 
-
-
-std::uint16_t
-fromBlockPosInChunkToHashValue(unsigned char x, unsigned char y, unsigned char z);
+std::uint16_t fromBlockPosInChunkToHashValue(unsigned char x, unsigned char y, unsigned char z);
 glm::ivec3 fromHashValueToBlockPosinChunk(std::uint16_t hashValue);
 
-//used for saving data
 struct BlockDataHeader
 {
-	//todo hold the version inside the pos y or something
 	glm::ivec3 pos = {};
 	BlockType blockType = 0;
 	std::uint16_t dataSize = 0;
@@ -22,30 +16,20 @@ struct BlockDataHeader
 
 struct BlocksWithDataHolder
 {
-
 	std::unordered_map<std::uint16_t, BaseBlock> baseBlocks;
 	std::unordered_map<std::uint16_t, ChestBlock> chestBlocks;
 
 	BaseBlock *getBaseBlock(unsigned char x, unsigned char y, unsigned char z);
 	BaseBlock *getOrCreateBaseBlock(unsigned char x, unsigned char y, unsigned char z);
-
-
 	ChestBlock *getChestBlock(unsigned char x, unsigned char y, unsigned char z);
 	ChestBlock *getOrCreateChestBlock(unsigned char x, unsigned char y, unsigned char z);
 
 	void formatBlockData(std::vector<unsigned char> &dataToAppend, int chunkXChunkSpace, int chunkZChunkSpace);
-
-	// Parses into a temporary holder and commits only after the whole payload is
-	// valid. This prevents a truncated/corrupt .block file from leaving a
-	// partially reconstructed chest/base-block state behind.
-	bool loadBlockData(std::vector<unsigned char> &data, int chunkXChunkSpace, int chunkZChunkSpace);
-
+	bool loadBlockData(const std::vector<unsigned char> &data, int chunkXChunkSpace, int chunkZChunkSpace);
 };
 
+void appendChestBlock(std::vector<unsigned char> &dataToAppend, glm::ivec3 position, ChestBlock &chestBlock);
 
-void appendChestBlock(std::vector<unsigned char> &dataToAppend,
-	glm::ivec3 position, ChestBlock &chestBlock);
-	
 struct InteractionData
 {
 	unsigned char blockInteractionType = 0;
